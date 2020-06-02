e86e72b0160922b8b89f3d332571e8db8d17ce97
Farrukh Atabekov <augini18@gmail.com>
2020-06-02 19:17:37 +0900

    --initial commit

--- src/components/Tab/TabCard/TabCard.js
+++ src/components/Tab/TabCard/TabCard.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText } from 'mdbreact';
+import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText} from 'mdbreact';
 import './TabCard.css'
 
 const TabCard = (props) => {
   return (
     <a href={props.content.url}>
       <MDBCard className="tab-card-container w-100 mb-1" style={{ maxWidth: "22rem", color: 'black' }} >
+      {/* <MDBCardImage className="img-fluid" src={props.content.urlToImage} waves /> */}
         <MDBCardBody>
           <MDBCardTitle>{props.content.title}</MDBCardTitle>
           <MDBCardText>{props.content.description}</MDBCardText>
