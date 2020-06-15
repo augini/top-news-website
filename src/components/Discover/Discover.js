563775f1b2d0b97f9d314f7b43f7b62a8b28daa9
Farrukh Atabekov <augini18@gmail.com>
2020-06-16 03:33:29 +0900

    -update: heroku deploy

--- src/components/Discover/Discover.js
+++ src/components/Discover/Discover.js
@@ -5,6 +5,6 @@ import "./DiscoverStyle.css"
 export const Discover = (props) => (
   <div>
     <h4 className = "discover-container"> DISCOVER MORE...</h4>
-    <DiscoverCardList generalContent = {props.generalContent.slice(12,16)}/>
+    <DiscoverCardList generalContent = {props.generalContent}/>
   </div>
 )
