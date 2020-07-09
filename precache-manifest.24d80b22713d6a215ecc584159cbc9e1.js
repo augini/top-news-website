8bc3408a777dcc23f87cd4c2aacf9492ab384636
Farrukh Atabekov <augini18@gmail.com>
2020-07-10 02:21:47 +0900

    Updates

---
+++ precache-manifest.24d80b22713d6a215ecc584159cbc9e1.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07ff5b90b3ad943b6bebe0d2584efe0b",
+    "url": "/top-news-website/index.html"
+  },
+  {
+    "revision": "01be70c26526839e0e86",
+    "url": "/top-news-website/static/css/2.2be2783e.chunk.css"
+  },
+  {
+    "revision": "ae551d128b5986e8c66a",
+    "url": "/top-news-website/static/css/main.0d169b7d.chunk.css"
+  },
+  {
+    "revision": "01be70c26526839e0e86",
+    "url": "/top-news-website/static/js/2.9ea770bf.chunk.js"
+  },
+  {
+    "revision": "8f1ab18056c81b97e85bb68b42e50a35",
+    "url": "/top-news-website/static/js/2.9ea770bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae551d128b5986e8c66a",
+    "url": "/top-news-website/static/js/main.0e921a12.chunk.js"
+  },
+  {
+    "revision": "aed0cbc74263e31fa350",
+    "url": "/top-news-website/static/js/runtime-main.d29dbfec.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/top-news-website/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/top-news-website/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/top-news-website/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/top-news-website/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/top-news-website/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/top-news-website/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/top-news-website/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/top-news-website/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/top-news-website/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/top-news-website/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/top-news-website/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/top-news-website/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/top-news-website/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/top-news-website/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/top-news-website/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/top-news-website/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/top-news-website/static/media/fa-solid-900.ec763292.svg"
+  },
+  {
+    "revision": "2434e4f0658fa694dc7c578e21950193",
+    "url": "/top-news-website/static/media/russia.2434e4f0.svg"
+  },
+  {
+    "revision": "5bd50db07092cd7116ea7e04aeaccfe2",
+    "url": "/top-news-website/static/media/south-korea.5bd50db0.svg"
+  },
+  {
+    "revision": "a035edadeadb79c859f4a31c4392b1bb",
+    "url": "/top-news-website/static/media/united-states-of-america.a035edad.svg"
+  }
+]);
